e1203f65be40866884e9bd0f60d229acd0ccefba
zhujun <zhw095@ucsd.edu>
2016-09-19 16:42:54 -0700

    first version

---
+++ Debugger/LeEcoDebugger/CrashFile.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LeEcoDebugger
+{
+    public class CrashFile
+    {
+        private ulong start = 0;
+        private ulong size = 0;
+        private ulong fileOffset = 0;
+        private FileStream stream = null;
+        private BinaryReader reader = null;
+        public string filename = string.Empty;
+        public ulong MemStart { get { return start; } }
+        public ulong FileOffset { get { return fileOffset; } }
+        public ulong MemSize { get { return size; } }
+        public string iName = string.Empty;
+        public Dictionary<string, RelocableType> reloc = new Dictionary<string, RelocableType>();
+        public CrashFile(string file, ulong memStart, string iName = "")
+        {
+            mapFile(file, memStart, 0, 0, iName);
+        }
+        public CrashFile(string file, ulong memStart, ulong offset, ulong length, string iName = "")
+        {
+            mapFile(file, memStart, offset, length, iName);
+        }
+
+        private void mapFile(string file, ulong memStart, ulong offset, ulong length, string iName = "")
+        {
+            try
+            {
+                if (Util.fileExists(file))
+                {
+                    filename = file;
+                    start = memStart;
+                    fileOffset = offset;
+                    size = length;
+                    if (length == 0)
+                    {
+                        FileInfo info = new FileInfo(file);
+                        size = (UInt64)info.Length - offset;
+                    }
+                }
+                this.iName = iName;
+            }
+            catch (Exception e)
+            { }
+        }
+
+        public bool readBufferExFromPA(UInt64 addr, UInt32 size, out byte[] buffer, List<CrashFile> dumpFile, string imageName)
+        {
+            buffer = null;
+            if (dumpFile == null)
+                return false;
+            uint current_size = 0;
+
+            MemoryStream ms = new MemoryStream();
+            foreach (CrashFile file in dumpFile)
+            {
+                if (file.iName.Equals(imageName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    current_size = (uint)(file.size - (addr - file.MemStart));
+                    var buff = file.read(addr, current_size);
+                    if (buff == null)
+                        buff = new byte[current_size];
+                    ms.Write(buff, 0, buff.Length);
+                }
+               
+            }
+            if (ms.Length == 0)
+                return false;
+            if (ms.Length >= size)
+            {
+                ms.Position = 0;
+                buffer = new byte[size];
+                ms.Read(buffer, 0, (int)size);
+            }
+            ms.Dispose();
+            return true;
+
+        }
+
+        public byte[] read(UInt64 addr, uint bytes)
+        {
+            byte[] buffer = null;
+            try
+            {
+                if (stream == null)
+                {
+                    stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    reader = new BinaryReader(stream);
+                }
+                reader.BaseStream.Seek((long)(addr - start + fileOffset), 0);
+                buffer = reader.ReadBytes((int)bytes);
+            }
+            catch (Exception e)
+            {
+                string msg = "debugger fail to access file";
+            }
+
+            return buffer;
+        }
+
+    }
+
+}
---
+++ Debugger/LeEcoDebugger/Util.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+namespace LeEcoDebugger
+{
+    class Util
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+        /// <summary>Determines if a file exists and is accessible, with a given Timeout</summary>
+        /// <param name="path">Path of file to check</param>
+        /// <param name="timeout">(optional) Timeout in milliseconds, default is 10 seconds</param>
+
+        public enum Endian { BigE, LittleE};
+        public static bool fileExists(string path, int timeout = 10000)
+        {
+            if (string.IsNullOrEmpty(path))
+                return false;
+            bool found = ShortTask.Execute<bool>(timeout, false, () =>
+            {
+                bool exists = false;
+                if (path.IndexOfAny(new char[] { '?', '*' }) >= 0)
+                {
+                    string folderPath = Path.GetDirectoryName(path);
+                    string filePath = Path.GetFileName(path);
+                    string[] files = null;
+                    try
+                    {
+                        if (Util.directoryExists(folderPath))
+                            files = Directory.GetFiles(folderPath, filePath, SearchOption.TopDirectoryOnly);
+                    }
+                    catch (Exception ex) { }
+                    exists = (files != null && files.Length != 0);
+                }
+                else
+                    exists = File.Exists(path);
+                return exists;
+            });
+
+            return found;             
+        }
+
+        public static bool directoryExists(string path, int timeout = 10000)
+        {
+            if (string.IsNullOrEmpty(path))
+                return false;
+            bool found = ShortTask.Execute<bool>(timeout, false, () =>
+            {
+                return Directory.Exists(path);
+            });
+            return found;
+                
+        }
+
+        public static string GetTempFolder()
+        {
+            return Environment.GetEnvironmentVariable("Temp");
+        }
+
+        public static string ExtractString(byte[] buffer, uint index, uint maxSize = 0)
+        {
+            if (buffer == null) return string.Empty;
+            if (maxSize == 0) maxSize = (uint)buffer.Length;
+            List<byte> list = new List<byte>();
+            uint i = index;
+            while (i < buffer.Length && buffer[i] != 0 && i - index < maxSize)
+            {
+                list.Add(buffer[i++]);
+                
+            }
+
+            return Encoding.UTF8.GetString(list.ToArray());
+        }
+
+        public static UInt32 ExtractUInt32(byte[] buffer, uint index, Endian endian = Endian.LittleE)
+        {
+            if (buffer == null) return 0;
+            if (buffer.Length - index < 4)
+                return 0;
+            if (endian == Endian.LittleE)
+            {
+                return (uint)(buffer[index + 3] << 24) + (uint)(buffer[index + 2] << 16) + (uint)(buffer[index + 1] << 8) + buffer[index]; 
+            }
+            else
+            {
+                return (uint)(buffer[index] << 24) + (uint)(buffer[index + 1] << 16) + (uint)(buffer[index + 2] << 8) + buffer[index + 3];
+            }
+        }
+    }
+}
